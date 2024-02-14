f3428d7b72f9ba5c4765d44a714ae55d25fded8d
Shaun Smiley <senorsmile@gmail.com>
2024-02-14 14:44:17 -0800

    fix move to nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,41 @@
-{ nixpkgs}:
-let
-  # Create a wrapper of terraform that has access to only the declared list of
-  # providers. Mixed-and-matched from both nixpkgs and this project.
-  my_python = nixpkgs.terraform.withPlugins (p: [
-    # The providers coming from nixpkgs have a flat namespace
-    # https://github.com/NixOS/nixpkgs/blob/nixos-22.11/pkgs/applications/networking/cluster/terraform-providers/providers.json
-    p.aws
-    p.null
+{
+  inputs = {
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:NixOS/nixpkgs/115a96e2ac1e92937cd47c30e073e16dcaaf6247"; # Apr 10, 2023 12:15 PM PDT (nixos-22.11)
+    #nixpkgs.url = "github:NixOS/nixpkgs/0f7f5ca1cdec8dea85bb4fa60378258171d019ad"; # May 29, 2023 3:30am PDT (nixos-23.05)
+    nixpkgs.url = "github:NixOS/nixpkgs/56911ef3403a9318b7621ce745f5452fb9ef6867"; # Jan 27, 2024 8:37am PS (nixos-23.11)
 
-    # for transfer_family module
-    #p.local
-    #p.archive
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-    # for terrablocks sftp module
-    p.random
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    }:
 
-    # dns made easy 
-    p.dme
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        my-python-packages = ps: with ps; [
+          boto3
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ansible
+            (python3.withPackages my-python-packages)
+          ];
 
-    # The providers coming from nixpkgs-terraform-providers-bin have a 1:1
-    # mapping with the terraform registry, replacing `/` with `.`:
-    # https://registry.terraform.io/providers/hashicorp/nomad
-    nixpkgs-terraform-providers-bin.providers.hashicorp.nomad
-  ]);
-in
-# Here we create a trivial shell with only that wrapper
-nixpkgs.mkShell {
-  packages = [
-    my_terraform
-  ];
+          #shellHook = with pkgs; ''
+          #  echo "packer `${packer}/bin/packer --version`"
+          #  ${terraform}/bin/terraform --version
+          #  ${nomad}/bin/nomad --version
+          #  ${vault}/bin/vault --version
+          #'';
+        };
+      }
+    );
 }
---
+++ run_ansible.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SITEFILE="${SITEFILE:-${SCRIPT_DIR}/site.yml}"
+GIT_EXIT_IF_CHANGES="${GIT_EXIT_IF_CHANGES:-yes}"
+
+opts=(
+  ansible-playbook          # must be first
+  -i "${SCRIPT_DIR}/inventory/"
+  --become
+  --diff
+  #--vault-password-file ./.ansible_vault.txt
+  "${SITEFILE}"  # must be last
+)
+
+export ANSIBLE_CALLBACKS_ENABLED='timer,profile_tasks'
+export ANSIBLE_STDOUT_CALLBACK='yaml'
+
+exit_if_git_changes() {
+  # exit if there are uncommited changes
+  if [[ $GIT_EXIT_IF_CHANGES == "yes" ]]; then
+    local git_changes=$(git diff-index HEAD --)
+    if [[ $git_changes == "" ]]; then
+      : # no changes
+    else
+      #set +u
+      #local $OVERRIDE_GIT_COMMIT_REQ="${OVERRIDE_GIT_COMMIT_REQ:-false}"
+      #if [[ $OVERRIDE_GIT_COMMIT_REQ != "TRUE" ]]; then
+        echo "UNCOMMITED CHANGES IN GIT!"
+        echo "Commit first then run again"
+        exit 1
+      #fi
+    fi
+  fi
+}
+  
+
+date
+exit_if_git_changes
+set -x
+time ${opts[@]} ${@}
+set +x
+date
